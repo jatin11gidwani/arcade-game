bbda644bd56e5e4fa4f1838f4e12e84d750fbbaa
Jatin Gidwani <jatin11gidwani@gmail.com>
2018-01-23 11:43:57 +0530

    Initial Commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -11,26 +13,126 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    "use strict";
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    if (this.x >= 500) {
+      this.speed = Math.floor(Math.random() * (120 - 40 + 1)) + 40;
+      this.x = -50;
+    }
+    this.collision();
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+    "use strict";
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Method to check the collision between enemy and player.
+Enemy.prototype.collision = function() {
+    "use strict";
+    // Set hitboxes for collision detection
+    var playerBox = {x: player.x, y: player.y, width: 50, height: 40};
+    var enemyBox = {x: this.x, y: this.y, width: 60, height: 70};
+    // Check for collisions, if playerBox intersects enemyBox, we have one
+    if (playerBox.x < enemyBox.x + enemyBox.width &&
+        playerBox.x + playerBox.width > enemyBox.x &&
+        playerBox.y < enemyBox.y + enemyBox.height &&
+        playerBox.height + playerBox.y > enemyBox.y) {
+        // // Collision detected, call collisionDetected function
+        // this.collisionDetected();
+        alert("You hit the enemy");
+        player.reset();
+    }
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function() {
+  "use strict";
+  this.startingX = 200;
+  this.startingY = 400;
+  this.x = this.startingX;
+  this.y = this.startingY;
+  this.sprite = 'images/char-boy.png'
+};
 
+Player.prototype.update = function() {
+    //Empty Method
+};
+
+// Resets the position of the player
+Player.prototype.reset = function() {
+    "use strict";
+    this.startingX = 200;
+    this.startingY = 400;
+    this.x = this.startingX;
+    this.y = this.startingY;
+};
+
+// Method to call alert and reset the game when player reaches the finish line
+Player.prototype.finished = function() {
+    "use strict";
+    alert('You won the game');
+    this.reset();
+};
+
+// Method to draw the player on the screen
+Player.prototype.render = function() {
+    "use strict";
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Move the player according to keys pressed
+Player.prototype.handleInput = function(allowedKeys) {
+    "use strict";
+    switch (allowedKeys) {
+        case "left":
+            //check for wall, otherwise move left
+            if (this.x > 0) {
+                this.x -= 101;
+            }
+            break;
+        case "right":
+            //check for wall, otherwise move right
+            if (this.x < 402) {
+                this.x += 101;
+            }
+            break;
+        case "up":
+            //check if player reached top of water, if so call finished function,
+            // otherwise move up
+            if (this.y < 0) {
+                this.finished();
+            } else {
+                this.y -= 83;
+            }
+            break;
+        case "down":
+            //check for bottom, otherwise move down
+            if (this.y < 400) {
+                this.y += 83;
+            }
+            break;
+    }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+let allEnemies = [];
 // Place the player object in a variable called player
+let player = new Player();
 
-
+// For loop to create enemies
+for (var i = 0; i < 3; i++) {
+    var startSpeed = Math.floor(Math.random() * (120 - 40 + 1)) + 40;
+    //enemys start off canvas (x = -100) at the following Y positions: 60, 145, 230
+    allEnemies.push(new Enemy(-100, 60 + (85 * i), startSpeed));
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -41,6 +143,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
